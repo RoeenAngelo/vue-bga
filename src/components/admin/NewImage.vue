1c14387f9beefd4b9fcc4dde2ecacfe076993a96
Roeen <roeenacal@gmail.com>
2023-07-26 15:31:37 -0700

    Add user must be signed in to upload an image

--- src/components/admin/NewImage.vue
+++ src/components/admin/NewImage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { getStorage, uploadBytes, uploadBytesResumable, ref as storageReference, getDownloadURL } from 'firebase/storage'
 import { useStoreAuth } from '../../stores/storeAuth';
 import { storeToRefs } from 'pinia';
@@ -8,91 +8,39 @@ const storeAuth = useStoreAuth()
 const { userData } = storeToRefs(storeAuth)
 
 const message = ref('')
-const newImage = ref([])
+const signInMessage =ref('')
 
-const uploadTask = ref(null)
-
-
+watch(userData, () => {
+    signInMessage.value = ''
+  })
 /*
   Upload New Image
 */
+
+
+
 function handleChange(e) {
     const storage = getStorage()
     const file = e.target.files[0]
     const storageRef = storageReference(storage, 'images/'+ file.name)
 
     try {
-      uploadBytes(storageRef, e.target.files[0]).then((snapshot) => {
+      if(userData.value){
+        uploadBytes(storageRef, e.target.files[0]).then((snapshot) => {
         message.value = 'Image Uploaded';
       });
+      } else{
+        signInMessage.value = 'Please sign in to upload an image.'
+      }
+
     } catch (error) {
       message.value = 'Image Upload Failed'
     }
 
-    // uploadTask.value = uploadBytesResumable(storageRef, e.target.files[0])
-    // uploadTask.value.on('state_changed', (snapshot) => {
-     
-    //     }, 
-    //     (error) => {
-    //       message.value = 'Upload Failed'
-    //     },
-    //     () => {
-    //       // Upload completed successfully, now we can get the download URL
-    //       getDownloadURL(uploadTask.value.snapshot.ref).then((downloadURL) => {
-    //         message.value = 'Image Uploaded';
-    //       });
-    //     }
-        
-    //     )
-  
   }
 
 
-  // function handleChange(e) {
-  //   const storage = getStorage()
-  //   const file = e.target.files[0]
-  //   const storageRef = storageReference(storage, 'images/'+ file.name)
-  //   uploadTask.value = uploadBytesResumable(storageRef, e.target.files[0])
-  //   uploadTask.value.on('state_changed', (snapshot) => {
-  //     // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-  //     progress.value = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-  //     message.value = 'Image Uploaded'
-  //     switch (snapshot.state) {
-  //       case 'paused':
-  //         console.log('Upload is paused');
-  //         break;
-  //       case 'running':
-  //         console.log('Upload is running');
-  //         break;
-  //         }
-  //       }, 
-  //       (error) => {
-  //         message.value = 'Upload Failed'
-  //       },
-  //       () => {
-  //         // Upload completed successfully, now we can get the download URL
-  //         getDownloadURL(uploadTask.value.snapshot.ref).then((downloadURL) => {
-  //           console.log('File available at', downloadURL);
-  //         });
-  //       }
-        
-  //       )
-  
-  //   paused.value = false
 
-  // }
-
-  // function pause() {
-  //   if(paused.value == false) {
-  //     uploadTask.value.pause
-  //     paused.value = true
-  //   }
-  //   else {
-  //     uploadTask.value.resume 
-  //     paused.value = false
-  //   }
-    
-  // }
 
 
 </script>
@@ -112,29 +60,14 @@ function handleChange(e) {
       </span>
     </label>
     <span class="message">{{ message }}</span>
+    <span v-if="signInMessage" class="sign-in-message">{{ signInMessage }}</span>
   </div>
-
-  <!-- <div>
-  <input type="file" @change="handleChange">
-  <button>
-    <i v-if="paused" class="fa-solid fa-pause"></i>
-    <i v-else class="fa-solid fa-play"></i>
-  </button>
-  </div> -->
-
 
 </template>
 
 
 
 <style lang="scss" scoped>
-
-// .admin_section {
-//   display: flex;
-//   width: 100%;
-//   justify-content: flex-end;
-//   max-width: 650px;
-// }
 
 .file {
   font-size: .6em;
@@ -145,6 +78,10 @@ function handleChange(e) {
   margin-left: 1rem;
   font-size: 1.1em;
   color: rgb(104, 205, 104);
+}
+
+.sign-in-message {
+  color: red;
 }
 
 </style>
