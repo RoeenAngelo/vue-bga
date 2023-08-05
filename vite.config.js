c9b891e8e40eab08e9df29f1731074ca214c19a5
Roeen <roeenacal@gmail.com>
2023-08-04 18:29:58 -0700

    Add dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-bga/",
+  base: '/vue-bga/',
   plugins: [vue()],
   resolve: {
     alias: {
